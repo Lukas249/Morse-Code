2d64b159006173c65df21b56e9c773a694f83621
Łukasz Radecki <lukaszradecki6@gmail.com>
2024-11-05 19:06:46 +0100

    chore: update return types from int? to int and adjust default parameter values in VibrationManager

--- lib/pages/encode/vibrations/vibration_manager.dart
+++ lib/pages/encode/vibrations/vibration_manager.dart
@@ -3,32 +3,32 @@ import 'package:vibration/vibration.dart';
 abstract class VibrationManagerAbstract {
   Future<void> vibrate({int duration, int amplitude});
 
-  Future<bool?> hasCustomVibrationsSupport();
+  Future<bool> hasCustomVibrationsSupport();
 
-  Future<bool?> hasVibrator();
+  Future<bool> hasVibrator();
 
   Future<void> cancel();
 }
 
 class VibrationManager implements VibrationManagerAbstract {
+
   @override
-  Future<void> cancel() async{
-     await Vibration.cancel();
+  Future<void> vibrate({int duration = 500, int amplitude = -1}) async {
+    await Vibration.vibrate(duration: duration, amplitude: amplitude);
   }
 
   @override
-  Future<bool?> hasCustomVibrationsSupport() async {
-    return await Vibration.hasCustomVibrationsSupport();
+  Future<bool> hasCustomVibrationsSupport() async {
+    return await Vibration.hasCustomVibrationsSupport() ?? false;
   }
 
   @override
-  Future<bool?> hasVibrator() async {
-    return await Vibration.hasVibrator();
+  Future<bool> hasVibrator() async {
+    return await Vibration.hasVibrator() ?? false;
   }
 
   @override
-  Future<void> vibrate({int? duration, int? amplitude}) async {
-    await Vibration.vibrate();
+  Future<void> cancel() async {
+    await Vibration.cancel();
   }
-
 }
