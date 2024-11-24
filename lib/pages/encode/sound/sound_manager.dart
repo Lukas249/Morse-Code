cb224723f0739320f4bfdd0430593bb684d8e26b
pyrczuu <dydekpyrcz@gmail.com>
2024-11-24 13:41:06 +0100

    feat: added sound encoder and dark theme

--- lib/pages/encode/sound/sound_manager.dart
+++ lib/pages/encode/sound/sound_manager.dart
@@ -1,3 +1,19 @@
-class SoundManager {
+import 'package:audioplayers/audioplayers.dart';
 
+abstract class SoundManagerAbstract{
+  Future<void> turnOnSound();
+  Future<void> turnOffSound();
+}
+
+class SoundManager implements SoundManagerAbstract {
+  final player = AudioPlayer();
+ // String source = "beep.mp3";
+  @override
+  Future<void> turnOnSound() async{
+    await player.play(AssetSource("beep.mp3"));
+  }
+  @override
+  Future<void> turnOffSound() async {
+    await player.stop();
+  }
 }
