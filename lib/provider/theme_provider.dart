3e8fc6d8bea9263e8a73e7d6776285cc23884eaa
Łukasz Radecki <lukaszradecki6@gmail.com>
2024-11-26 21:25:29 +0100

    feat: create ThemeData for light and dark theme

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,16 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+ThemeData lightTheme = ThemeData.light().copyWith(
+    colorScheme: const ColorScheme.light().copyWith(
+        primary: Colors.lightBlue,
+        onPrimary: Colors.white,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.black
+    )
+);
+
+ThemeData darkTheme = ThemeData.dark().copyWith(
+    colorScheme: const ColorScheme.dark().copyWith(
+        primary: Colors.lightBlue,
+        onPrimary: Colors.white,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white
+    )
+);
+
 class ThemeProvider extends ChangeNotifier {
-  ThemeData _theme = ThemeData.light();
+  ThemeData _theme = lightTheme;
+
   ThemeData get theme => _theme;
 
   void toggleTheme() {
-    final isDark = _theme == ThemeData.dark();
+    final isDark = _theme == darkTheme;
     if (isDark) {
-      _theme = ThemeData.light();
+      _theme = lightTheme;
     } else {
-      _theme = ThemeData.dark();
+      _theme = darkTheme;
     }
     notifyListeners();
   }
